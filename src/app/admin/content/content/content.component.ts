b3c65e460e3d9954682b2f5a522fc06189b48a02
Jan Nowak <jannowak@example.com>
2018-11-05 00:16:30 +0100

    rework1

--- src/app/admin/content/content/content.component.ts
+++ src/app/admin/content/content/content.component.ts
@@ -11,72 +11,74 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
- mode;
- contentId;
- contentForm;
- type = ['text','text_and_image','gallery', 'contact']
- bodyForm: FormGroup;
+  mode;
+  contentId;
+  contentForm;
+  type = ['text', 'text_and_image', 'gallery', 'contact']
+  bodyForm: FormGroup;
   constructor(public contenstService: ContentService,
     private _fb: FormBuilder,
     public route: ActivatedRoute) {
-      this.initComp()
-      this.buildForm(null)
-      // this.buildForm()
-     }
+    this.initComp()
+    this.buildForm(null)
+    // this.buildForm()
+  }
 
-    
+
   ngOnInit() {
-}
-initComp(){
-  console.log('init',this.contentForm)
-  this.route.paramMap.subscribe((paramMap: ParamMap) => {
-    if (paramMap.has("contentId")) {
-      this.mode = "edit";
-      this.contentId = paramMap.get("contentId");
-      this.contenstService.getContent(this.contentId).subscribe(res => {
-        this.contentForm = res;
-        this.buildForm(this.contentForm)
-        
-        console.log(this.contentForm)
-        
-        // this.bodyForm.patchValue(this.contentForm);
-      });
-    } else {
-      this.mode = "create";
-      this.contentId = null;
-      this.buildForm(null)
-    }
-  });
-}
-buildForm(data:any): FormGroup{
-  console.log(data)
-return this.bodyForm = this._fb.group({
-  type: [data? data.type:'',],
-  tag: [data? data.tag:'',],
-  styles: this._fb.group({
-    text_type: [data? data.styles.text_type:'',]
-  }),
-  content: this._fb.array(
-    this.getContent(data? data.content:null)
-  )
-})
-}
-getContent(content:any): FormGroup[]{
-  return content? content.map(contentBody=>{
-     return this._fb.group({
-      title: [ contentBody? contentBody.title: '' ],
-      lead: [ contentBody.lead],
-      text: [ contentBody.text],
-      signature: [ contentBody.signature],
-      button: [ contentBody.button],
-      date: [ contentBody.date],
-      reference: [ contentBody.reference],
-      decriptions: [ contentBody.decriptions],
-      image: this._fb.array(
-        this.getImage(contentBody? contentBody.image:null)
-      )
+  }
+  initComp() {
+    console.log('init', this.contentForm)
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has("contentId")) {
+        this.mode = "edit";
+        this.contentId = paramMap.get("contentId");
+        this.contenstService.getContent(this.contentId).subscribe(res => {
+          this.contentForm = res;
+          this.buildForm(this.contentForm)
+
+          console.log(this.contentForm)
+
+          // this.bodyForm.patchValue(this.contentForm);
+        });
+      } else {
+        this.mode = "create";
+        this.contentId = null;
+        this.buildForm(null)
+      }
     });
-  }):[this._fb.group({
+  }
+  buildForm(data: any): FormGroup {
+    console.log(data)
+    return this.bodyForm = this._fb.group({
+      type: [data ? data.type : '',],
+      tag: [data ? data.tag : '',],
+      styles: this._fb.group({
+        text_type: [data ? data.styles.text_type : '',]
+      }),
+      content: this._fb.array(
+        this.getContent(data ? data.content : null)
+      )
+    })
+  }
+  getContent(content: any[]) {
+    console.log("get",content)
+    return content ? content.map(contentBody => {
+      return this._fb.group({
+        title: [contentBody.title],
+        lead: [contentBody.lead],
+        text: [contentBody.text],
+        signature: [contentBody.signature],
+        button: [contentBody.button],
+        date: [contentBody.date],
+        reference: [contentBody.reference],
+        decriptions: [contentBody.decriptions],
+        image: this._fb.array(
+          this.getImage(contentBody ? contentBody.image : null)
+        )
+      });
+    }) : 
+    [this._fb.group({
       title: '',
       text: '',
       lead: '',
@@ -88,28 +90,84 @@ getContent(content:any): FormGroup[]{
       image: this._fb.array(
         this.getImage(null)
       )
-  })]
-}
-getImage(image:any[]): FormGroup[]{
-  return image? image.map(imageBody=>{
-    return this._fb.group({
-      url: [imageBody.url],
-      title: [imageBody.title]
-    });
-}):[this._fb.group({
-  url: '',
-  title: ''
-})]
-}
-addC(x){
-  // let control = <FormArray>this.bodyForm.controls.content;
-  //    control.push(
-  //     this._fb.group({
-  //       content: this._fb.array([
-  //         this._fb.group({text: ''})
-  //       ])
-  //     })
-  //   )
+    })]
+    // [
+    //   new FormGroup({
+    //     title: new FormControl,
+    //     text: new FormControl,
+    //   lead: new FormControl,
+    //   signature: new FormControl,
+    //   button: new FormControl,
+    //   date: new FormControl,
+    //   reference: new FormControl,
+    //   decriptions: new FormControl,
+    //   image: this._fb.array(
+    //         this.getImage(null)
+    //       )
+    //   })
+    // ]
 
+  }
+  getImage(image: any[]): FormGroup[] {
+    return image ? image.map(imageBody => {
+      return this._fb.group({
+        url: [imageBody.url],
+        title: [imageBody.title]
+      });
+    }) : [this._fb.group({
+      url: '',
+      title: ''
+    })]
+  }
+  addC(content?: Content) {
+    console.log("1",this.bodyForm);
+    (<FormArray>this.bodyForm.controls['content']).push(
+      new FormGroup({
+        title: new FormControl,
+        text: new FormControl,
+      lead: new FormControl,
+      signature: new FormControl,
+      button: new FormControl,
+      date: new FormControl,
+      reference: new FormControl,
+      decriptions: new FormControl,
+      image: this._fb.array(
+            this.getImage(null)
+          )
+      })
+    )
+    console.log("2",this.bodyForm)
+    // let control = <FormArray>this.bodyForm.controls.content;
+    // console.log(control)
+    // control.push(
+    //   this._fb.array([
+    //     this._fb.group({
+    //       title: 'dupa'
+    //     })
+    //   ])
+    // )
+    // let control = <FormGroup>this.bodyForm.controls.content;
+    // console.log(control)
+    // control.push(
+    //   this._fb.array([
+    //     this._fb.group({
+    //       title: 'dupa'
+    //     })
+    //   ])
+    // )
+    // const control = <FormArray>this.bodyForm.controls['content'];
+    // console.log(control)
+    // console.log("form",this.getContent(null))
+    // control.push(new FormArray(this.getContent(null)))
+    // this.bodyForm.controls.content.push(this.getContent(null))
+    // let control1 = <FormArray>this.bodyForm.controls.content;
+    // let control = (<FormArray>this.bodyForm.controls['content'])
+    // let answers = this.bodyForm.get(`content.${0}`) as FormArray;
+    // answers.push(
+    //   this._fb.array([])
+    // )
+    // console.log(answers)
+    // console.log("1",control1)
+    // console.log(control)
   }
 }
