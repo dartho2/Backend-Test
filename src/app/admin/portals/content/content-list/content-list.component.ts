ef1b4f7c075309b180b5e92781dc3208228aef06
Jan Nowak <jannowak@example.com>
2018-11-29 23:58:55 +0100

    patch 6.0.0.2

--- src/app/admin/portals/content/content-list/content-list.component.ts
+++ src/app/admin/portals/content/content-list/content-list.component.ts
@@ -12,7 +12,7 @@ import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
   templateUrl: './content-list.component.html',
   styleUrls: ['./content-list.component.css']
 })
-export class ContentComponent implements OnInit, OnDestroy, OnChanges {
+export class ContentComponent implements OnInit {
   
   selectedId
   portals: Portal[];
@@ -34,43 +34,12 @@ export class ContentComponent implements OnInit, OnDestroy, OnChanges {
     this.route.paramMap.subscribe(param => {
       this.contentChangePosition = null;
       this.sectionID = param.get('contentID')
-      // this.contentID =false
-      // this.ContentToSection = false
       this.portalService.getSection(this.sectionID).subscribe(response => {
-        // this.dragAndDrop = null
-        window.clearInterval(this.dragAndDrop)
-        
-        this.dragAndDrop = this.content = response
-        
-        // this.drop(this.dragAndDrop);
+        this.content = response
+        this.dragAndDrop = this.content
       })
-      // if (this.portals) {
-      //   this.sections = this.portals.filter(x => x._id === this.portalID)
+    })
 
-      // }
-    }
-    )
-    // this.portalService.getPortals();
-    // this.portalsSub = this.portalService.getPortalsUpdatedListener()
-    //   .subscribe((portals: Portal[]) => {
-    //     this.portals = portals;
-    //   });
-    // this.route.parent.paramMap.subscribe(x => {
-    //   this.portalID = x.get('sectionID')
-
-    // })
-
-    // this.route.paramMap.subscribe(x => {
-      
-    //   this.sectionID = x.get('contentID')
-    //   this.contentID =false
-    //   this.ContentToSection = false
-    //   if (this.portals) {
-    //     this.sections = this.portals.filter(x => x._id === this.portalID)
-
-    //   }
-    // }
-    // )
   }
   drop(event: CdkDragDrop<any[]>) {
       moveItemInArray(this.dragAndDrop.data, event.previousIndex, event.currentIndex);
@@ -86,42 +55,10 @@ export class ContentComponent implements OnInit, OnDestroy, OnChanges {
     this.contentID = id
   }
   saveContentPosition(){
-    // console.log(this.sectionID,'sectiokn id || content',this.dragAndDrop
-    // )
     this.portalService.changePositionSections(this.sectionID, this.dragAndDrop)
     this.contentChangePosition = null;
-
-    // this.portalService.changePosition(this.portal, this.contentChangePosition)
   }
-  // goDown(id) {
-  //   let a;
-  //   let c: string;
-  //   this.portalService.getSection(this.sectionID).subscribe(x => {
-  //     a = x
-  //     var szukaIDsekcji = a.data.findIndex(x => x === id)
-  //     if (a.data.length > szukaIDsekcji + 1) {
-  //       c = a.data.splice(szukaIDsekcji, 1)
-  //       a.data.splice(szukaIDsekcji + 1, 0, c[0])
-  //       this.portalService.changePositionSections(this.sectionID, a)
-  //     }
-  //   })
-
-  // }
-
-  // goUp(id) {
-  //   let a;
-  //   let c: string;
-  //   this.portalService.getSection(this.sectionID).subscribe(x => {
-  //     a = x
-  //     var szukaIDsekcji = a.data.findIndex(x => x === id)
-  //     if (0 < szukaIDsekcji) {
-  //       c = a.data.splice(szukaIDsekcji, 1)
-  //       a.data.splice(szukaIDsekcji - 1, 0, c[0])
-  //       this.portalService.changePositionSections(this.sectionID, a)
-  //     }
-  //   })
-
-  // }
+ 
   contentDelete(id){
     if(confirm("Are you sure to delete "+id)) {
       this.portalService.deleteContent(id).subscribe(() => {
@@ -129,15 +66,6 @@ export class ContentComponent implements OnInit, OnDestroy, OnChanges {
           this.content = response
         })
     })
-
     }
-  }
-  ngOnChanges(){
-    console.log('zmiana')
-  } 
-  ngOnDestroy() {
-    // this.contentChangePosition = null
-    console.log('destroy')
-    // this.change.unsubscribe();
   }
 }
